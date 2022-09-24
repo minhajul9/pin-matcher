fa18172324c122e86b8bb00e3f63fcb42d0a7eb8
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2022-09-24 15:04:28 +0600

    pin generate

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-var x = Math.random();
-x = x*9999;
-console.log(Math.round(x));
+console.log('hello');
+document.getElementById('generateButton').addEventListener('click', function(){
+    var num = Math.random()*9999;
+    num = Math.round(num) +'';
+    if(num.length==1){
+        num = num+'000';
+    }
+    else if(num.length==2){
+        num = num+'00';
+    }
+    else if(num.length==3){
+        num = num+'0';
+    }
+    document.getElementById('generateField').value = num;
+}) ;
